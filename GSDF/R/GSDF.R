03c396424e728e22afcfb92d8b3155af253f6ddb
Philip Brohan <philip@brohan.org>
2014-12-12 11:48:46 +0000

    Added 'greedy' option (defaults to false) to all the interpolation and regridding functions to allow estimation of values where some of the neighbouring points to interpolate from are missing data. (Required for sst and sea-ice fields, which otherwise are set to missing along coasts).

--- GSDF/R/GSDF.R
+++ GSDF/R/GSDF.R
@@ -112,9 +112,12 @@ GSDF.roll.dimensions<-function(g,d1,d2) {
 #' @param g GSDF data structure
 #' @param x values to interpolate to in 1st dimension
 #' @param y values to interpolate to in 2nd dimension
+#' @param greedy boolean. If FALSE (default) NA values in the input field
+#'      propagate to generate NA values in all output values adjacent to them.
+#'      If true, output values are NA only if all adjacent grid points are NA.
 #' @return numeric vector of interpolated values
 #' @seealso \code{\link{GSDF.regrid.2d}} regrid 1 field to match another
-GSDF.interpolate.2d<-function(g,x,y) {
+GSDF.interpolate.2d<-function(g,x,y,greedy=FALSE) {
    if(length(x) != length(y)) stop("Mismatch in interpolation points")
    # Must have exactly 2 extended dimensions
    dims<-GSDF.get.extended.dimensions(g)
@@ -123,9 +126,9 @@ GSDF.interpolate.2d<-function(g,x,y) {
    }
    grid<-array(data=g$data,dim=c(length(g$dimensions[[dims[1]]]$values),
                                  length(g$dimensions[[dims[2]]]$values)))
-   interp<-interp.surface(list(x=g$dimensions[[dims[1]]]$values,
-                               y=g$dimensions[[dims[2]]]$values,z=grid),
-                          cbind(x,y))
+   interp<-GSDF.interp.surface(list(x=g$dimensions[[dims[1]]]$values,
+                                    y=g$dimensions[[dims[2]]]$values,z=grid),
+                                    cbind(x,y),greedy=greedy)
    return(interp)
 }
 
@@ -145,8 +148,11 @@ GSDF.interpolate.2d<-function(g,x,y) {
 #'   global lat:lon field (wrap longitudes and extrapolate near poles if required,
 #'   FALSE => just interpolate (removes data outside range of g),
 #'   NULL (default) => Guess which to do (is field ll and covering most of globe?).
+#' @param greedy boolean. If FALSE (default) NA values in the input field
+#'      propagate to generate NA values in all output values adjacent to them.
+#'      If true, output values are NA only if all adjacent grid points are NA.
 #' @return numeric vector of interpolated values
-GSDF.interpolate.ll<-function(g,lat,lon,full=NULL) {
+GSDF.interpolate.ll<-function(g,lat,lon,full=NULL,greedy=FALSE) {
    dims<-GSDF.get.extended.dimensions(g)
    if(length(dims)!=2) {
       stop('Must have exactly 2 extended dimensions')
@@ -161,7 +167,7 @@ GSDF.interpolate.ll<-function(g,lat,lon,full=NULL) {
                                                   lon,g$dimensions[[dims[2]]]$type)
         g<-GSDF.pad.longitude(g)
       }
-       return(GSDF.interpolate.2d(g,lat,lon))
+       return(GSDF.interpolate.2d(g,lat,lon,greedy=greedy))
      } else stop("Field has no longitudes")
    }
    if(g$dimensions[[dims[1]]]$type=='lon') {
@@ -173,7 +179,7 @@ GSDF.interpolate.ll<-function(g,lat,lon,full=NULL) {
                                                   lon,g$dimensions[[dims[1]]]$type)
         g<-GSDF.pad.longitude(g)
       }
-       return(GSDF.interpolate.2d(g,lon,lat))
+       return(GSDF.interpolate.2d(g,lon,lat,greedy=greedy))
      } else stop("Field has no latitudes")
    }
    stop("Field is not lat:lon")
@@ -193,8 +199,11 @@ GSDF.interpolate.ll<-function(g,lat,lon,full=NULL) {
 #'   global lat:lon field (wrap longitudes and extrapolate near poles if required,
 #'   FALSE => just interpolate (removes data outside range of g.grid),
 #'   NULL (default) => Guess which to do (is field ll and covering most of globe?).
+#' @param greedy boolean. If FALSE (default) NA values in the input field
+#'      propagate to generate NA values in all output values adjacent to them.
+#'      If true, output values are NA only if all adjacent grid points are NA.
 #' @return GSDF field with data from g on grid of g.grid
-GSDF.regrid.2d<-function(g,g.grid,full=NULL) {
+GSDF.regrid.2d<-function(g,g.grid,full=NULL,greedy=FALSE) {
    dims<-GSDF.get.extended.dimensions(g)
    if(length(dims)!=2) {
       stop('Field must have exactly 2 extended dimensions')
@@ -251,7 +260,7 @@ GSDF.regrid.2d<-function(g,g.grid,full=NULL) {
         g<-GSDF.pad.longitude(g)
       }  
       result$data<-array(data=GSDF.interpolate.2d(g,new.x,new.y),
-                         dim=dim(g.grid$data))
+                         dim=dim(g.grid$data),greedy=greedy)
       return(result)
    }
    if(g$dimensions[[dims[1]]]$type == g.grid$dimensions[[dims.grid[2]]]$type &&
@@ -291,7 +300,7 @@ GSDF.regrid.2d<-function(g,g.grid,full=NULL) {
         g<-GSDF.pad.longitude(g)
       }  
       result$data<-array(data=GSDF.interpolate.2d(g,new.x,new.y),
-                        dim=dim(g.grid$data))
+                        dim=dim(g.grid$data),greedy=greedy)
       return(result)
    }
    stop("Incompatable fields - different dimensions")
@@ -848,4 +857,77 @@ GSDF.addleap<-function(d) {
    d$data<-d1
    d$dimensions[[t.d]]$values<-dim1
    return(d)
+}
+
+#' 2d bilinear interpolation
+#'
+#' Identical to interp.surface from the fields package. Except
+#'  that it optionally ignores NA values in the source field.
+#'
+#' Useful for regridding sea-ice and SST fields, which otherwise
+#'  end up as missing along coastlines.
+#'
+#' @export
+#' @param obj A list with components x,y, and z in the same style as used
+#'            by contour, persp, image etc. x and y are the X and Y grid
+#'            values and z is a matrix with the corresponding values of the
+#'            surface.
+#' @param loc A matrix of (irregular) locations to interpolate. First
+#'            column of loc is the X coordinates and second is the Y's.
+#' @param greedy Boolean - if TRUE (default) NA are returned for regions of
+#'          the obj\$z where any neighbour points are NA (same as version in fields).
+#'          If FALSE, only return NA where all neighbouring points are NA.
+#' @return An vector of interpolated values. 
+GSDF.interp.surface<-function(obj,loc.greedy=FALSE) {
+    x <- obj$x
+    y <- obj$y
+    z <- obj$z
+    nx <- length(x)
+    ny <- length(y)
+    lx <- approx(x, 1:nx, loc[, 1])$y
+    ly <- approx(y, 1:ny, loc[, 2])$y
+    if(greedy) {
+        lx <- approx(x, 1:nx, loc[, 1],rule=2)$y
+        ly <- approx(y, 1:ny, loc[, 2],rule=2)$y
+    }  
+    lx1 <- floor(lx)
+    ly1 <- floor(ly)
+    ex <- lx - lx1
+    ey <- ly - ly1
+    ex[lx1 == nx] <- 1
+    ey[ly1 == ny] <- 1
+    lx1[lx1 == nx] <- nx - 1
+    ly1[ly1 == ny] <- ny - 1
+    if(!greedy) {
+        return(z[cbind(lx1, ly1)] * (1 - ex) * (1 - ey) + z[cbind(lx1 + 
+            1, ly1)] * ex * (1 - ey) + z[cbind(lx1, ly1 + 1)] * (1 - 
+            ex) * ey + z[cbind(lx1 + 1, ly1 + 1)] * ex * ey)
+    } else {
+      result<-rep(0,length(x))
+      weight<-rep(0,length(x))
+      w<-which(!is.na(z[cbind(lx1, ly1)]))
+      if(length(w)>0) {
+        weight[w]<-weight[w]+(1 - ex[w]) * (1 - ey[w])
+        result[w]<-result[w]+z[cbind(lx1, ly1)][w]* (1 - ex[w]) * (1 - ey[w])
+      }
+      w<-which(!is.na(z[cbind(lx1+1, ly1)]))
+      if(length(w)>0) {
+        weight[w]<-weight[w]+ ex[w] * (1 - ey[w])
+        result[w]<-result[w]+z[cbind(lx1+1, ly1)][w]* ex[w] * (1 - ey[w])
+      }
+      w<-which(!is.na(z[cbind(lx1, ly1+1)]))
+      if(length(w)>0) {
+        weight[w]<-weight[w]+(1 - ex[w]) * ey[w]
+        result[w]<-result[w]+z[cbind(lx1, ly1+1)][w]* (1 - ex[w]) * ey[w]
+      }
+      w<-which(!is.na(z[cbind(lx1+1, ly1+1)]))
+      if(length(w)>0) {
+        weight[w]<-weight[w]+ ex[w] * ey[w]
+        result[w]<-result[w]+z[cbind(lx1+1, ly1+1)][w]* ex[w] * ey[w]
+      }
+      w<-which(weight==0)
+      is.na(result[w])<-TRUE
+      result[-w]<-result[-w]/weight[-w]
+      return(result)
+    }
 }
