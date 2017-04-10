dbfadf52d3a6361f6e51830d904ffc88743fd358
Philip Brohan <philip@brohan.org>
2017-04-10 13:46:02 +0100

    Functional, but untested, version of GSDF.time.

--- GSDF/R/GSDF.time.R
+++ GSDF/R/GSDF.time.R
@@ -19,16 +19,40 @@
 GSDF.time<-function(string,calendar) {
   result<-list()
   result$date<-string
-  result$calendar<-calendar
+  result$calendar<-GSDF.time.check.calendar(calendar)
+  ck<-GSDF.time.check.time(result)
+  return(result)
+}
+
+#' Check that the calendar is supported
+#'
+#' Can do gregorian, 360_day and 365_day calendars.
+#'
+#' 365_day may be called 'noleap'
+#' gregorian may be called 'standard' or
+#' 'proleptic_gregorian'.
+#'
+#' @param date A celendar string
+#' @result One of 'gregorian', '360_day' and '365_day'
+GSDF.time.check.calendar<-function(calendar) {
+
+  calendar<-tolower(calendar)
+  if(calendar=='standard' ||
+     calendar=='preleptic_gregorian') {
+    calendar<-'gregorian'
+  }
+  if(calendar=='noleap') {
+    calendar<-'365_day'
+  }
   if(calendar != 'gregorian' &&
      calendar != '360_day'   &&
      calendar != '365_day') {
     stop(sprintf("Unsupported calendar %s",calendar))
   }
-  ck<-GSDF.check.times(result)
-  return(result)
-}
+  return(calendar)
 
+}
+  
 #' Check that dates are meaningful
 #'
 #' Check that each string date is valid
@@ -37,12 +61,7 @@ GSDF.time<-function(string,calendar) {
 #'
 #' @param date A GSDF time object
 #' @result TRUE if all the contents are valid.
-GSDF.time.check<-function(date) {
-  if(date$calendar != 'gregorian' &&
-     date$calendar != '360_day'   &&
-     date$calendar != '365_day') {
-    stop(sprintf("Unsupported calendar %s",date$calendar))
-  }
+GSDF.time.check.time<-function(date) {
   w<-which(!grepl("\\d\\d\\d\\d-\\d\\d-\\d\\d:\\d\\d:\\d\\d",
                   date$date))
   if(length(w)==1) {
@@ -66,18 +85,17 @@ GSDF.time.check<-function(date) {
     }
   }
   m<-stringr::str_match(date$date,
-            "(\\d\\d\\d\\d)-(\\d\\d)-(\\d\\d:\\d\\d:\\d\\d)")
-  years<-m[,2]
-  months<-m[,3]
-  days<-m[,4]
-  hours<-m[,5]
-  minutes<-m[,6]
+            "(\\d\\d\\d\\d)-(\\d\\d)-(\\d\\d):(\\d\\d):(\\d\\d)")
+  years   <- as.integer(m[,2])
+  months  <- as.integer(m[,3])
+  days    <- as.integer(m[,4])
+  hours   <- as.integer(m[,5])
+  minutes <- as.integer(m[,6])
   if(date$calendar=='360_day') {
      w<-which(months<1   | months>12 |
               days<1     | days>30   |
               hours>23   |
-              minutes>59 |
-              seconds>59)
+              minutes>59 )
     if(length(w)==1) {
     stop(sprintf("%s is not a valid 360_day date",
                  date$date[w]))
@@ -88,12 +106,11 @@ GSDF.time.check<-function(date) {
     }
   }
   if(date$calendar=='365_day') {
-    month.lengths=c(31,28,31,30,31,30,31,31,30,31,30,31)
+    month.length=c(31,28,31,30,31,30,31,31,30,31,30,31)
      w<-which(months<1   | months>12 |
               days<1     | days>month.length[months] |
               hours>23   |
-              minutes>59 |
-              seconds>59)
+              minutes>59 )
     if(length(w)==1) {
     stop(sprintf("%s is not a valid 365_day date",
                  date$date[w]))
@@ -106,6 +123,191 @@ GSDF.time.check<-function(date) {
   
   return(TRUE)   
 
+}
+
+#' Add a period to a GSDF.time
+#'
+#' Increment the time by a number of years, month, days,
+#'  hours or minutes.
+#'
+#' Increments can be negative, and can be fractional.
+#'
+#' @export
+#' @param date - GSDF.time
+#' @param units Units of the offset - string 'hours', 'days', ...
+#' @param offset numeric vector of time offsets.
+#' @Result A list containing two components
+GSDF.time.increment<-function(date,offset,units) {
+
+  if(units != 'years' && units != 'months' && units != 'days' &&
+     units != 'hours' && units != 'minutes') {
+    stop(sprintf("%s is not a supported time offset unit %s",
+          units,
+         "(expected 'years', 'months','days', 'hours, or 'minutes')"))
+  }
+  # Cope with different length of date and offset
+  #if(length(date$date)!=length(offset)) {
+  #  if(length(date$date)!=1 && length(offset)!=1) {
+  #    stop(sprintf("Length mismatch: %d dates and %d offsets",
+  #                 length(date$date),length(offset)))
+  #  }
+  #  if(length(date$date)==1) {
+  #    date$date<-rep(date$date,length(offset))
+  #  }
+  #  if(length(offset==1)) {
+  #    offset<-rep(offset,length(date$date))
+  #  }
+  #}
+
+  # Split the string times into components
+  m<-stringr::str_match(date$date,
+           "(\\d\\d\\d\\d)-(\\d\\d)-(\\d\\d):(\\d\\d):(\\d\\d)")
+  year  <- as.integer(m[,2])
+  month <- as.integer(m[,3])
+  day   <- as.integer(m[,4])
+  hour  <- as.integer(m[,5])
+  minute<- as.integer(m[,6])
+
+  if(date$calendar=='gregorian') {
+     l.base<-lubridate::ymd_hms(sprintf("%04d-%02d-%02d:%02d:%02d:00",
+                             year,month,day,hour,minute))
+     if(units=='years') {
+       if(any(as.integer(offset)!=offset)) {
+         stop("Increments in units of years must be integers")
+       }
+       l.base<-l.base+lubridate::years(as.integer(offset))
+     }
+     if(units=='months') {
+       if(any(as.integer(offset)!=offset)) {
+         stop("Increments in units of months must be integers")
+       }
+       # Months have variable length - choose here to just
+       #  change the month, leave day, hour etc. unchanged.
+       l.base<-lubridate::"%m+%"(l.base,months(offset))
+     }
+     if(units=='days') {
+       l.base<-l.base+lubridate::days(as.integer(offset))
+       offset<-(offset-as.integer(offset))*24
+       units<-'hours'
+     }
+     if(units=='hours') {
+       l.base<-l.base+lubridate::hours(as.integer(offset))
+       offset<-(offset-as.integer(offset))*60
+       units<-'minutes'
+     }
+     if(units=='minutes') {
+       l.base<-l.base+lubridate::minutes(as.integer(offset))
+     }
+     string.times<-sprintf("%04d-%02d-%02d:%02d:%02d",
+                            lubridate::year(l.base),
+                            lubridate::month(l.base),
+                            lubridate::day(l.base),
+                            lubridate::hour(l.base),
+                            lubridate::minute(l.base))
+     return(GSDF.time(string.times,date$calendar))
+   }
+  
+   if(date$calendar=='360_day') {
+     if(units=='years') {
+       if(any(as.integer(offset)!=offset)) {
+         stop("Increments in units of years must be integers")
+       }
+       year<-year+offset
+     }
+     if(units=='months') {
+       if(any(as.integer(offset)!=offset)) {
+         stop("Increments in units of months must be integers")
+       }
+       month<-month+offset
+       year<-year+as.integer((month-1)/12)
+       month<-month%%12
+       w<-which(month==0)
+       if(length(w)>0) month[w]<-12
+     }
+     if(units=='days') {
+        offset<-offset*24*60
+        units<-'minutes'
+     }
+     if(units=='hours') {
+        offset<-offset*60
+        units<-'minutes'
+    }
+     if(units=='minutes') {
+        minute<-minute+as.integer(offset)
+        hour<-as.integer(minute/60)
+        minute<-minute%%60
+        day<-as.integer(hour/24)
+        hour<-hour%%24
+        month<-month+as.integer((day-1)/30)
+        day<-day%%30
+        w<-which(day==0)
+        if(length(w)>0) day[w]<-30
+        year<-year+as.integer((month-1)/12)
+        month<-month%%12+1
+        w<-which(month==0)
+        if(length(w)>0) month[w]<-12
+ 
+     }
+     string.times<-sprintf("%04d-%02d-%02d:%02d:%02d",
+                            as.integer(year),
+                            as.integer(month),
+                            as.integer(day),
+                            as.integer(hour),
+                            as.integer(minute))
+     return(GSDF.time(string.times,date$calendar))
+   }
+  
+   if(date$calendar=='365_day') {
+     if(units=='years') {
+       if(any(as.integer(offset)!=offset)) {
+         stop("Increments in units of years must be integers")
+       }
+       year<-year+offset
+     }
+     if(units=='months') {
+       if(any(as.integer(offset)!=offset)) {
+         stop("Increments in units of months must be integers")
+       }
+       month<-month+offset
+       year<-year+as.integer((month-1)/12)
+       month<-month%%12+1
+     }
+     if(units=='days') {
+       ofset<-offset*24*60
+        units<-'minutes'
+     }
+     if(units=='hours') {
+        offset<-offset*60
+        units<-'minutes'
+     }
+     month.lengths<-c(31,28,31,30,31,30,31,31,30,31,30,31)
+     month.tostart<-c(0,31,59,90,120,151,181,212,243,273,304,334,365)
+     if(units=='minutes') {
+        julian<-month.tostart[month]*24*60+day*24*60+as.integer(offset)
+        year<-year+as.integer(julian/(365*24*60))
+        julian<-julian%%(365*24*60)
+        for(m in seq(1,12)) {
+          w<-which(julian>=month.tostart[m]*24*60 &
+                   julian<month.tostart[m+1]*24*60)
+          if(length(w)==0) next
+          month[w]<-m
+          julian[w]<-julian[w]-month.tostart[m]*24*60
+          day[w]<-as.integer(julian[w]/(24*60))+1
+          julian[w]<-julian[w]%%(24*60)
+          hour[w]<-as.integer(julian[w]/60)
+          julian[w]<-julian[w]%%(60)
+          minute[w]<-as.integer(julian[w])
+        }
+     }
+     string.times<-sprintf("%04d-%02d-%02d:%02d:%02d",
+                            as.integer(year),
+                            as.integer(month),
+                            as.integer(day),
+                            as.integer(hour),
+                            as.integer(minute))
+     return(GSDF.time(string.times,date$calendar))
+   }
+  
 }
 
 #' Convert base + offset into GSDF.time format
@@ -135,40 +337,14 @@ GSDF.time.from.base.and.offset<-function(offset,base,units,calendar) {
     stop(sprintf("%s is not a valid base time (expected YYYY-MM-DD + optional:HH:MM)",
                  base))
   }
-  m<-stringr::str_match(base,"(\\d\\d\\d\\d)\D(\\d\\d)\D(\\d\\d)\D(\d\d)")
+  m<-stringr::str_match(base,"(\\d\\d\\d\\d)\\D(\\d\\d)\\D(\\d\\d)\\D(\\d\\d)")
   hour<-m[,5]
   if(is.na(hour)) hour<-0
   minute<-m[,6]
   if(is.na(minute)) minute<-0
-
-  if(calendar=='gregorian') {
-
-     l.base<-lubridate::ymd_hms(sprintf("%04d-%02d-%02d:%02d:%02d:00",
-                             year,month,day,hour,minute))
-    
-     if(units=='days') {
-       l.base<-l.base+lubridate::days(as.integer(offset))
-       offset<-(offset-as.integer(offset))*24
-       units<-'hours'
-     }
-     if(units=='hours') {
-       l.base<-l.base+lubridate::hours(as.integer(offset))
-       offset<-(offset-as.integer(offset))*60
-       units<-'minutes'
-     }
-     if(units=='minutes') {
-       l.base<-l.base+lubridate::minutes(as.integer(offset))
-     } else {
-       stop("%s is not a supported time offset unit (expected 'days', 'hours, or 'minutes')",
-            units)
-     }
-     string.times<-sprintf(("%04d-%02d-%02d:%02d:%02d:00",
-                            lubridate::year(l.base),
-                            lubridate::month(l.base),
-                            lubridate::day(l.base),
-                            lubridate::hour(l.base),
-                            lubridate::minute(l.base)))
-     return(GSDF.time(string.times,calendar))
-   }
-  
+  base<-GSDF.time(sprintf("%04d-%02d-%02d:%02d:%02d:00",
+                             year,month,day,hour,minute),calendar)
+  result<-GSDF.time.increment(base,offset,units)
+  return(result)
 }
+
